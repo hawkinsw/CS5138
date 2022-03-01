3b4653b96da7144a320e7a6e458e358b520a843e
Will Hawkins <whh8b@obs.cr>
2022-02-28 20:06:01 -0500

    NASM example.

---
+++ examples/asm/README.md
@@ -0,0 +1,21 @@
+## Hello, ASM
+
+A simple Hello, World! program written in nothing but ASM.
+
+### Compiling
+
+```
+make
+```
+
+### Running
+
+```
+$ ./hello.out
+```
+
+### Expected Result
+
+```
+Hello, ASM
+```
---
+++ examples/asm/hello.asm
@@ -0,0 +1,15 @@
+        global _start
+        extern puts
+
+        section .data
+hello:  db "Hello, ASM!", 10
+
+        section .text
+_start: mov rdi, hello ; the first parameter to `puts` is the string to print.
+                       ; Calling conventions say that 1st param always goes
+											 ; in rdi
+        call puts      ; call puts
+
+        mov rax, 60    ; 60 is the system call for exit
+        xor rdi, rdi   ; set the return value of the process to 0.
+        syscall        ; now, tell the OS that we actually want to do a syscall.
